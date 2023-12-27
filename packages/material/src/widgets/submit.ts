27bf8e86e50064fc6aab02d08f841c8df248de07
Julian Cataldo <contact@juliancataldo.com>
2023-12-28 00:48:55 +0100

    chore: giving a try with `prettier-plugin-sort-imports`

--- packages/material/src/widgets/submit.ts
+++ packages/material/src/widgets/submit.ts
@@ -8,7 +8,7 @@ export const submit: Widgets['submit'] = (options) => html`
 	<!--  -->
 	<div id=${options.id} class="theme-material widget-submit">
 		<md-filled-button type="submit"
-			>${options.submitButtonText ?? 'Submit'}</md-filled-button
+			>${options.label ?? 'Submit'}</md-filled-button
 		>
 	</div>
 `;
